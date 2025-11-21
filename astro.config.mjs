3721e2abd14c67f8d5fa3821f27cefefd347bcd3
Sefu <dumitru.10.patrick@gmail.com>
2025-11-22 00:07:52 +0200

    Updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  site: 'www.roca-inot.xyz', // optional, dar e bine
+  site: 'https://www.roca-inot.xyz', // optional, dar e bine
   base: '/RoCa',                        // <- ESENȚIAL pt Pages subfolder
   vite: { plugins: [tailwindcss()] }
 })
