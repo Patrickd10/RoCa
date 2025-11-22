b8aac24c0922657a5f2bf0c06558108f1afef2c1
Sefu <dumitru.10.patrick@gmail.com>
2025-11-22 22:56:04 +0200

    First deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  site: 'https://www.roca-inot.xyz', // sau https://www.roca-inot.xyz/ dacă folosești www
+  site: 'https://www.roca-swimming.ro', // sau https://www.roca-inot.xyz/ dacă folosești www
   base: '/',                         // IMPORTANT
   vite: { plugins: [tailwindcss()] }
 });
